84ab9d6e50ddc79bed306269e5b0b1691f43eff5
YASH_SHAH <120025464+Yashshah4536@users.noreply.github.com>
2024-08-10 16:52:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,13 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
 
+# Install project dependencies
+pip install -r requirements.txt
+
+# Convert static asset files
+python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python manage.py makemigrations
+python manage.py migrate
